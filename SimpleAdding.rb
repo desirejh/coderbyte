944c1830fcf6ba8717c523a1e3f58cd7ccf23439
desirejh <lovejacker1201@gmail.com>
2015-07-31 01:47:07 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,12 @@
+def SimpleAdding(num)
+
+  # code goes here
+  #반대로 계속 더하는 코드....?
+  return  +  (1..num).inject(:+)
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
+
+
